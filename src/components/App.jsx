b3f6255efea4da434270a2eac5eeddc47e44939f
adnanahmedbilalbn <adnanahmedbilalbn@gmail.com>
2023-03-02 17:51:09 +0500

    props and data-mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Footer from "./Footer";
 import Heading from "./Heading";
-import Note from "./Note";
+import Notes from "../notes.js"
+import Note from "./Note"
+
+
+
 
 function App (){
     return(
     <div>
         <Heading/>
-        <Note/>
+
+            {Notes.map( (props)=> {
+
+                return (
+                  <Note 
+                      key = {props.key}
+                      title = {props.title}
+                      content = {props.content}
+                  />
+                )
+            })}
+     
         <Footer />
     </div>
     )
